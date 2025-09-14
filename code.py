ef9b8c2dfab4901313fc037617666b809c3fcce5
edigavenendra <edigavenendra@gmail.com>
2025-09-14 11:37:41 +0530

    python.1

--- code.py
+++ code.py
@@ -1,1 +1,46 @@
-print("Hello world")
+a=10
+b=20
+print(a+b)
+
+print(9)
+
+print(9.5)
+
+a=10
+print(a)
+
+greeting ="good morining"
+print(greeting )
+print('hello world')
+
+#import demo
+import keyword 
+print(keyword.kwlist)
+
+#Identifiers
+data ="hello"
+#2daydata ="hello" -> invalid,as it start with digit
+_2daydata ="hello"
+
+a=20 
+A=30
+
+print(a)
+print(A)
+
+
+#class = "some data" --> keyword cannot be identifers
+#print(class)
+
+# define some student data 
+# bay way of using identifiers
+x ="chinna"
+y =25
+z = "python course"
+
+
+# Good way of using identifiers
+student_firstname = "chinna"
+student_age = 25 
+student_course ="Python course"
+
